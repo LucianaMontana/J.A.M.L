9e9dee8af9ae95452003abdb849234c0f5be0df4
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-05-09 21:21:17 -0300

    Agregando CSS

--- src/login.js
+++ src/login.js
@@ -9,14 +9,14 @@ function login() {
           <div className="md:w-1/3">
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Gmail"></input>
+            <input className="palta bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Gmail"></input>
           </div>
         </div>
         <div className="loginForm md:flex md:items-center mb-6">
           <div className="md:w-1/3">
           </div>
           <div className="md:w-2/3">
-            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="Contraseña"></input>
+            <input className="palta bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="Contraseña"></input>
           </div>
         </div>
         <div className="loginForm md:flex md:items-center mb-6">
