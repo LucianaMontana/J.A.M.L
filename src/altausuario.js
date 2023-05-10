e3f92502ec0e49f2656dc5ba54c07ac7d5a844a2
https://github.com/Antoluz05 <antosambuezaepet20@gmail.com>
2023-05-09 22:33:38 -0300

    Creando estilo del boton enviar

--- src/altausuario.js
+++ src/altausuario.js
@@ -22,8 +22,8 @@ function AltaUsuario() {
       
     </div>
     <div className="flex items-center justify-between">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Sign In
+      <button className="Boton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Enviar
       </button>
       <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
         Forgot Password?
