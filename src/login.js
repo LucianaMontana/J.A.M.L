960b62f909931cbbda271fbf5faa9000e46f9ba4
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-05-10 02:12:25 -0300

    Arreglando login

--- src/login.js
+++ src/login.js
@@ -9,14 +9,21 @@ function login() {
           <div className="md:w-1/3">
           </div>
           <div className="md:w-2/3">
-            <input className="palta bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Gmail"></input>
+            <input className="palta paltita input1 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Ingrese Gmail"></input>
           </div>
         </div>
         <div className="loginForm md:flex md:items-center mb-6">
           <div className="md:w-1/3">
           </div>
           <div className="md:w-2/3">
-            <input className="palta bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="Contraseña"></input>
+            <form className="form1">
+            <input className="palta paltita input1 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="Ingrese Contraseña"></input>
+            <label className="gmail1">
+                <span className="gmail2">
+                    Ingrese Gmail
+                </span>
+            </label>
+            </form>
           </div>
         </div>
         <div className="loginForm md:flex md:items-center mb-6">
