1b4559b53b127318b1de086ac3a193dc2c26c62f
https://github.com/Antoluz05 <antosambuezaepet20@gmail.com>
2023-05-09 23:06:01 -0300

    Arreglando errores

--- src/altausuario.js
+++ src/altausuario.js
@@ -4,12 +4,12 @@ import './App.css';
 function AltaUsuario() {
   return(
 <div class="w-full max-w-xs">
-  <form className =" alta bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <form className ="alta bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
     <div className="mb-4">
      
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Nombre"></input>
+      <input className="class shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Nombre"></input>
     </div>
-    <div classNameN="mb-6">
+    <div className="mb-6">
       <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="Apellido"  placeholder="Apellido"></input>
       
     </div>
@@ -25,8 +25,10 @@ function AltaUsuario() {
       <button className="Boton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
         Enviar
       </button>
+    </div>
+    <div>
       <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
+        Tenes una cuenta?
       </a>
     </div>
   </form>
