5e7b70547892985777a8525d5522b46df9ef407c
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-05-10 03:10:36 -0300

    Corrigiendo el login

--- src/login.js
+++ src/login.js
@@ -9,7 +9,14 @@ function login() {
           <div className="md:w-1/3">
           </div>
           <div className="md:w-2/3">
+          <form className="form1">
             <input className="palta paltita input1 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Ingrese Gmail"></input>
+            <label className="gmail1">
+                <span className="gmail2">
+                    
+                </span>
+            </label>
+            </form>
           </div>
         </div>
         <div className="loginForm md:flex md:items-center mb-6">
@@ -20,7 +27,7 @@ function login() {
             <input className="palta paltita input1 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="Ingrese Contraseña"></input>
             <label className="gmail1">
                 <span className="gmail2">
-                    Ingrese Gmail
+                    
                 </span>
             </label>
             </form>
@@ -40,14 +47,6 @@ function login() {
           <div className="md:w-2/3">
             <button className="boton1 boton2 shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
               Enviar
-            </button>
-          </div>
-        </div>
-        <div className="md:flex md:items-center">
-          <div className="md:w-1/3"></div>
-          <div className="md:w-2/3">
-            <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
-              
             </button>
           </div>
         </div>
