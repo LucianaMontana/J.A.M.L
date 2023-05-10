aaf076a68e337b4f302b681550bfa9560968e91c
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-05-09 22:20:50 -0300

    Arreglando errores del boton

--- src/login.js
+++ src/login.js
@@ -31,7 +31,7 @@ function login() {
         <div className="md:flex md:items-center">
           <div className="md:w-1/3"></div>
           <div className="md:w-2/3">
-            <button className="boton shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+            <button className="boton boton2 shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
               Enviar
             </button>
           </div>
