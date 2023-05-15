e5d2f9bdce0657e43f23380958ae15e1accf19af
MayTardugno <maylentardug@gmail.com>
2023-05-15 10:00:26 -0300

    guardando cambios

--- ReactProjects/my-app/src/componentes/practica.js
+++ ReactProjects/my-app/src/componentes/practica.js
@@ -1,26 +1,12 @@
-var React = require('react');
-var InputItem = require('');
-const { render } = require('react-dom');
+import React from 'react';
+import button from '';
 
-var InputBox = React.CreateClass({  
-    onClick: function(event){
-        console.log();
-    }
-
-
-      render: function cerrarSesion(){
-          return(
-              <div className='CerrarSesion'>
-                <InputItem/>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={this.onClick}>
-                 Button
-                </button>
-             </div>
-          );
-      }
-  }
-);
-
-
-module.exports = InputBox;
-
+function name(params) {
+  return(
+    <div className='boton'>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
+      Button
+      </button>  
+    </div>
+  ) 
+}
