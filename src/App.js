91058754738bb5252202f20bfead46f06b63aa5d
LucianaMontana <lucianam.epet20@gmail.com>
2023-08-14 00:04:35 -0300

    Empezando a autenticar usarios con firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import './App.css';
-import AltaUsuario from './altausuario';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Login from './login';
+import React from 'react';
 import InicioSesion from './componentes/login/InicioSesion';
 import Registrarse from './componentes/registro/Registrarse';
 import Home from './componentes/home/Home';
 
+import { app } from './componentes/firebase';
+
+
 function App() {
 
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<InicioSesion/>} />
-        <Route path='/registro' element={<Registrarse/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path= '/AltaUsuario' element={<AltaUsuario/>}/>
-        <Route path='/login' element={<Login />}/>
-      </Routes>
-    </Router>
+  const [ usuario, setUsuario ] = React.useState(null);
 
+  return (
+    <>
+      {usuario ? <Home/> : <InicioSesion/>}
+    </>
   );
 }
 export default App;
