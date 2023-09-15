85d0977bee6aef21862b5b47c7025a7c9e6955c6
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-09-15 20:49:20 -0300

    Creando function registrarte

--- src/pages/altausuario.js
+++ src/pages/altausuario.js
@@ -6,3 +6,15 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
+export default function Registrate() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+ 
+}
