786d06e8c807a2c5166af4a43cff7573ecf707ef
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-09-15 20:48:02 -0300

    Importando link, grid, box, typography

--- src/pages/altausuario.js
+++ src/pages/altausuario.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
