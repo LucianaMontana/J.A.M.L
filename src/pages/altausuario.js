528a2335e6cce861b63b57fdb71dfd496f6cf16e
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-09-15 20:51:57 -0300

    Creando formulario para registrarse

--- src/pages/altausuario.js
+++ src/pages/altausuario.js
@@ -15,6 +15,37 @@ export default function Registrate() {
       password: data.get('password'),
     });
   };
-
- 
-}
+  
+    return (
+      <Grid
+        container
+        className='background-image'
+        component='main'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <Grid
+          item
+          container
+          xs={4}
+          className='login-box'
+          sx={{
+            boxShadow: 3,
+            borderRadius: 2,
+            px: 4,
+            py: 6,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component='h1' variant='h5'>
+            Registrate
+          </Typography>
+          <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            
+          </Box>
+        </Grid>
+      </Grid>
+    );
+  }
