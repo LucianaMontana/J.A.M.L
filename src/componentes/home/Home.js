65f129315866362a59bdd8031fdf9badd41c4223
LucianaMontana <lucianam.epet20@gmail.com>
2023-09-16 02:45:15 -0300

    Logica cerrar Sesion

--- src/componentes/home/Home.js
+++ src/componentes/home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+// Credencial de Firebase
+import { app } from '../firebase';
 
 export default function Home() {
+
+    const cerrarSesion = () => {
+        app.auth().signOut();
+    }
+    
     return (
         <div>
-            <h1>Bienvenido</h1>
+            <h1>Bienvenido, Sesion Iniciada</h1>
+            <button onClick={cerrarSesion}>Cerrar Sesion</button>
         </div>
     )
 }
