82373f5ed55aa2ed563e59b5d9b8594368a07d4a
https://github.com/Antoluz05 <antosambuezaepet20@gmail.com>
2023-09-16 15:06:34 -0300

    Importando grid

--- src/componentes/OlvidoContraseña.js
+++ src/componentes/OlvidoContraseña.js
@@ -2,3 +2,4 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
+import { Grid } from '@mui/material';
