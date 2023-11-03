3065ad0cab948ea242a9a8dc080639a77f0a5c95
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-11-03 09:53:33 -0300

    merge

--- src/componentes/logueo/Logueo.js
+++ src/componentes/logueo/Logueo.js
@@ -10,27 +10,26 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-<<<<<<< HEAD
-import { app } from '../firebase';
-
-function Logueo({ isUserLoggedIn, setIsUserLoggedIn }) {
-  const [isRegistrando, setIsRegistrando] = React.useState(false);
-=======
 import { InputAdornment, IconButton } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 // Credencial de Firebase
-import {app} from "../firebase";
+import { app } from '../firebase';
 // Google
-import {auth, providerGoogle, providerGithub} from "../firebase";
+import { auth, providerGoogle, providerGithub } from '../firebase';
 import { signInWithPopup } from 'firebase/auth';
 // Home
 import Home from '../home/Home';
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+    <Typography
+      variant='body2'
+      color='text.secondary'
+      align='center'
+      {...props}
+    >
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color='inherit' href='https://mui.com/'>
         J.A.M.L
       </Link>{' '}
       {new Date().getFullYear()}
@@ -42,10 +41,8 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function Logueo(props) {
-
-  const [ isRegistrando, setIsRegistrando ] = React.useState(false);
+  const [isRegistrando, setIsRegistrando] = React.useState(false);
   // Usuario Anonimo
->>>>>>> 3851217e10385c2b452250a06e3005d089e8d2a5
   const [email, setEmail] = React.useState('');
   const [emailError, setEmailError] = React.useState('');
   const [password, setPassword] = React.useState('');
@@ -59,7 +56,7 @@ export default function Logueo(props) {
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
-  }
+  };
 
   const crearUsuario = (email, password) => {
     app
@@ -81,7 +78,7 @@ export default function Logueo(props) {
 
   const submitHandler = (e) => {
     e.preventDefault();
-  
+
     //Validacion de correo electronico
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
@@ -93,7 +90,7 @@ export default function Logueo(props) {
     setEmailError('');
 
     //Validacion de la logitud de la contraseña
-    if (password.length < 6 || password.length > 8 ) {
+    if (password.length < 6 || password.length > 8) {
       setPasswordErorr('La contraseña debe tener entre 6 y 8 caracteres.');
       return;
     }
@@ -111,138 +108,97 @@ export default function Logueo(props) {
   };
 
   // Boton Google
-  const IniciarSesionGoogle =() => {
-      signInWithPopup(auth, providerGoogle).then((dataGoogle)=>{ 
-        setUserGoogle(dataGoogle.user.email);
-        localStorage.setItem("email", dataGoogle.user.email);
-      })
-  }
+  const IniciarSesionGoogle = () => {
+    signInWithPopup(auth, providerGoogle).then((dataGoogle) => {
+      setUserGoogle(dataGoogle.user.email);
+      localStorage.setItem('email', dataGoogle.user.email);
+    });
+  };
 
   React.useEffect(() => {
     setUserGoogle(localStorage.getItem('email'));
-  })
+  });
 
   // Boton Github
-  const IniciarSesionGithub =() => {
-      signInWithPopup(auth, providerGithub).then((dataGithub)=>{ 
-        setUserGithub(dataGithub.user.email);
-        localStorage.setItem("email", dataGithub.user.email);
-      })
-  }
+  const IniciarSesionGithub = () => {
+    signInWithPopup(auth, providerGithub).then((dataGithub) => {
+      setUserGithub(dataGithub.user.email);
+      localStorage.setItem('email', dataGithub.user.email);
+    });
+  };
 
   React.useEffect(() => {
     setUserGithub(localStorage.getItem('email'));
-  })
+  });
 
   return (
-<<<<<<< HEAD
-    <ThemeProvider theme={createTheme()}>
-      <Container component='main' maxWidth='xs'>
-=======
-    <div className="background-container">
-     <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
->>>>>>> 3851217e10385c2b452250a06e3005d089e8d2a5
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            {isRegistrando ? 'Registrate' : 'Iniciar Sesion'}
-          </Typography>
-<<<<<<< HEAD
+    <div className='background-container'>
+      <ThemeProvider theme={defaultTheme}>
+        <Container component='main' maxWidth='xs'>
+          <CssBaseline />
           <Box
-            component='form'
-            onSubmit={submitHandler}
-            noValidate
-            sx={{ mt: 1 }}
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
           >
-=======
-          <Box component="form" onSubmit={submitHandler}  noValidate sx={{ mt: 1 }}>
-            {isRegistrando && (
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component='h1' variant='h5'>
+              {isRegistrando ? 'Registrate' : 'Iniciar Sesion'}
+            </Typography>
+            <Box
+              component='form'
+              onSubmit={submitHandler}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              {isRegistrando && (
+                <TextField
+                  margin='normal'
+                  required
+                  fullWidth
+                  id='username'
+                  label='Nombre y Apellido'
+                  name='username'
+                  autoComplete='username'
+                  autoFocus
+                />
+              )}
               <TextField
-                margin="normal"
+                margin='normal'
                 required
                 fullWidth
-                id="username"
-                label="Nombre y Apellido"
-                name="username"
-                autoComplete="username"
-                autoFocus    
+                id='email'
+                label='Correo'
+                name='email'
+                autoComplete='email'
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type='email'
+                error={!!emailError}
+                helperText={emailError}
               />
-            )}
->>>>>>> 3851217e10385c2b452250a06e3005d089e8d2a5
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              id='email'
-              label='Correo'
-              name='email'
-              autoComplete='email'
-              autoFocus
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              type='email'
-              error={!!emailError}
-              helperText={emailError}
-            />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-<<<<<<< HEAD
-              name='password'
-              label='Contraseña'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-=======
-              name="password"
-              label="Contraseña"
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              autoComplete="current-password"
->>>>>>> 3851217e10385c2b452250a06e3005d089e8d2a5
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              error={!!passwordError}
-              helperText={passwordError}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position='end' >
-                    <IconButton
-                      aria-label='toggle password visibility'
-                      onClick={togglePasswordVisibility}
-                      edge='end'
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                )
-              }}
-            />
-            {isRegistrando && (
               <TextField
-                margin="normal"
+                margin='normal'
                 required
                 fullWidth
-                id="recordar-password"
-                label="Recordar Contraseña"
+                name='password'
+                label='Contraseña'
                 type={showPassword ? 'text' : 'password'}
-                name="recordar-password"
-                autoComplete="recordar-password"
+                id='password'
+                autoComplete='current-password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                error={!!passwordError}
+                helperText={passwordError}
                 InputProps={{
                   endAdornment: (
-                    <InputAdornment position='end' >
+                    <InputAdornment position='end'>
                       <IconButton
                         aria-label='toggle password visibility'
                         onClick={togglePasswordVisibility}
@@ -251,89 +207,114 @@ export default function Logueo(props) {
                         {showPassword ? <Visibility /> : <VisibilityOff />}
                       </IconButton>
                     </InputAdornment>
-                  )
+                  ),
                 }}
               />
-            )}
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}
-            >
-              {isRegistrando ? 'Registrate' : 'Iniciar Sesion'}
-            </Button>
-            {userGoogle ? (<Home/>) :  
-              (!isRegistrando && (
-                <Button
-                  type="submit"
-                  variant="contained"
+              {isRegistrando && (
+                <TextField
+                  margin='normal'
+                  required
                   fullWidth
-                  sx={{ mt: 1, mb: 1 }}
-                  onClick={IniciarSesionGoogle}
-                >
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    x='0px'
-                    y='0px'
-                    width='25'
-                    height='25'
-                    viewBox='0 0 48 48'
+                  id='recordar-password'
+                  label='Recordar Contraseña'
+                  type={showPassword ? 'text' : 'password'}
+                  name='recordar-password'
+                  autoComplete='recordar-password'
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <IconButton
+                          aria-label='toggle password visibility'
+                          onClick={togglePasswordVisibility}
+                          edge='end'
+                        >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              )}
+              <Button
+                type='submit'
+                fullWidth
+                variant='contained'
+                sx={{ mt: 3, mb: 2 }}
+              >
+                {isRegistrando ? 'Registrate' : 'Iniciar Sesion'}
+              </Button>
+              {userGoogle ? (
+                <Home />
+              ) : (
+                !isRegistrando && (
+                  <Button
+                    type='submit'
+                    variant='contained'
+                    fullWidth
+                    sx={{ mt: 1, mb: 1 }}
+                    onClick={IniciarSesionGoogle}
                   >
-                    <path
-                      fill='#FFC107'
-                      d='M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z'
-                    ></path>
-                    <path
-                      fill='#FF3D00'
-                      d='M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z'
-                    ></path>
-                    <path
-                      fill='#4CAF50'
-                      d='M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z'
-                    ></path>
-                    <path
-                      fill='#1976D2'
-                      d='M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z'
-                    ></path>
-                  </svg>
-                  Google
-                </Button>
-              ))
-            }
-            {userGithub ? (<Home/>) :  
-              (!isRegistrando && (
-                <Button
-                  type="submit"
-                  variant="contained"
-                  fullWidth
-                  sx={{ mt: 1, mb: 1 }}
-                  onClick={IniciarSesionGithub}
-                >
-                  Github
-                </Button>
-              ))
-            }
-            <Grid container>
-              <Grid item>
-                <Link
-                  onClick={() => setIsRegistrando(!isRegistrando)}
-                  variant='body2'
-                  style={{ cursor: 'pointer' }}
-                >
-                  {isRegistrando
-                    ? '¿Ya tienes cuenta? Inicia Sesion'
-                    : '¿No tienes cuenta? Registrate'}
-                </Link>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      x='0px'
+                      y='0px'
+                      width='25'
+                      height='25'
+                      viewBox='0 0 48 48'
+                    >
+                      <path
+                        fill='#FFC107'
+                        d='M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z'
+                      ></path>
+                      <path
+                        fill='#FF3D00'
+                        d='M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z'
+                      ></path>
+                      <path
+                        fill='#4CAF50'
+                        d='M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z'
+                      ></path>
+                      <path
+                        fill='#1976D2'
+                        d='M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z'
+                      ></path>
+                    </svg>
+                    Google
+                  </Button>
+                )
+              )}
+              {userGithub ? (
+                <Home />
+              ) : (
+                !isRegistrando && (
+                  <Button
+                    type='submit'
+                    variant='contained'
+                    fullWidth
+                    sx={{ mt: 1, mb: 1 }}
+                    onClick={IniciarSesionGithub}
+                  >
+                    Github
+                  </Button>
+                )
+              )}
+              <Grid container>
+                <Grid item>
+                  <Link
+                    onClick={() => setIsRegistrando(!isRegistrando)}
+                    variant='body2'
+                    style={{ cursor: 'pointer' }}
+                  >
+                    {isRegistrando
+                      ? '¿Ya tienes cuenta? Inicia Sesion'
+                      : '¿No tienes cuenta? Registrate'}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
     </div>
-    
   );
 }
-
-export default Logueo;
