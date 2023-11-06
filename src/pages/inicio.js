f7d77ce99936e48ca55a1fa5d192d9886362c3df
MayTardugno <maylentardug@gmail.com>
2023-11-06 14:57:26 -0300

    haciendo cambios

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -6,9 +6,13 @@ function Inicio(){
     return(
         <>    
              <div>
+                <h1>Bienvenido a </h1>
                 <h1>Living Slate</h1>
             </div>              
             <div class="spinner"></div>
+            <div>
+                <h2>Cargando...</h2>
+            </div> 
         </>  
     );
 }
