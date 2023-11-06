db48400480254b25951969195be3294dd25a0a9d
MayTardugno <maylentardug@gmail.com>
2023-11-06 14:51:44 -0300

    arreglando pantalla de inicio

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -13,24 +13,4 @@ function Inicio(){
     );
 }
 
-/*                
- <div class="spinner-overlay">  
-        <div class="spinner"></div>
- </div>
-            // Para mostrar el spinner de carga
-function showLoader(){
-       document.querySelector('.spinner-overlay').style.display = 'block';
-}
-
-// Para ocultar el spinner de carga
-function hideLoader(){
-     document.querySelector('.spinner-overlay').style.display = 'none';
-}
-
-// Ejemplo de uso: Mostrar el spinner durante 3 segundos y luego ocultarlo
-showLoader();
-
-setTimeout(function(){
-        hideLoader();
-}, 3000); // Ejemplo de tiempo de carga de 3 segundos*/
 export default Inicio;
