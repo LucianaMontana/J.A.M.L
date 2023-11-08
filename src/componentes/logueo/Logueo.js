5f0074bb450e050e1eabe60cba80506cafbc74c6
https://github.com/Antoluz05 <antosambuezaepet20@gmail.com>
2023-11-08 20:24:14 -0300

    Agregando una texgtfield

--- src/componentes/logueo/Logueo.js
+++ src/componentes/logueo/Logueo.js
@@ -296,6 +296,7 @@ export default function Logueo(props) {
                 )}
                 <Grid item container xs={16} sx={{ display: 'flex' }}></Grid>
                 <Grid item sm={6} sx={{ marginRight: '2%' }}></Grid>
+                <TextField
 
                 <FormControlLabel
                   style={{
