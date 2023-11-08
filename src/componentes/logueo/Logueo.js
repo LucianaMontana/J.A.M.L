622d626e0c4c865e60cf3ca56c21c42609c46d46
https://github.com/Antoluz05 <antosambuezaepet20@gmail.com>
2023-11-08 20:25:11 -0300

    Agregando un required

--- src/componentes/logueo/Logueo.js
+++ src/componentes/logueo/Logueo.js
@@ -298,6 +298,7 @@ export default function Logueo(props) {
                 <Grid item sm={6} sx={{ marginRight: '2%' }}></Grid>
                 <TextField
                  margin='normal'
+                 required
 
                 <FormControlLabel
                   style={{
