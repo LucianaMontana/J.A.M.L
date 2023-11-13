249abad0c88138f833fc1262eb46237f8c8258bd
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-11-13 14:32:24 -0300

    Feat: OlvidoContraseña - Alineando el contenido del grid

--- src/componentes/OlvidoContraseña.js
+++ src/componentes/OlvidoContraseña.js
@@ -15,7 +15,13 @@ function OlvidoContraseña() {
     alert(`Se ha enviado un correo de recuperación a tu Gmail${email}`);
   };
   return (
-    <Grid container className='background-image' component='main'>
+    <Grid
+      container
+      className='background-image'
+      component='main'
+      justifyContent='center'
+      alignItems='center'
+    >
       <Grid>
         <h2>¿Has olvidado tu contraseña?</h2>
         <p>
