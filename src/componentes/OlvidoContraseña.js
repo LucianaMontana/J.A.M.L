ae1b9231529fd19d995cc05c30b18de1ddaf1f7a
https://github.com/LopezJeremias <lopezjeremias2004@gmail.com>
2023-11-13 14:24:19 -0300

    Feat: OlvidoContraseña - Creando funcion para la recuperacion de contraseña

--- src/componentes/OlvidoContraseña.js
+++ src/componentes/OlvidoContraseña.js
@@ -3,3 +3,10 @@ import Button from '@mui/material/Button';
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from 'react';
 import { Grid } from '@mui/material';
+
+function OlvidoContraseña() {
+  const [email, setEmail] = useState('');
+
+  return <div></div>;
+}
+export default OlvidoContraseña;
